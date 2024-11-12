1b877cb07c4aa47a9b248255c60dc9cbe12603cd
NahidKakon <nahidshakib@outlook.com>
2024-11-12 14:05:56 +0000

    Update ProjectsSection.jsx
    Signed-off-by: NahidKakon <nahidshakib@outlook.com>

--- app/sections/ProjectsSection.jsx
+++ app/sections/ProjectsSection.jsx
@@ -60,9 +60,9 @@ function ProjectsSection() {
        <Button className="text-black bg-orange-400
         bg-orange-4500 border hover:bg-orange-600 hover:border-orange-400">
          <Link
-          href="my-shop-nine-roan.vercel.app" className="button">My e-commerce shop <br/> (under construction)         </Link>
+          href="my-shop-nine-roan.vercel.app" className="button">My e-commerce shop under construction)         </Link>
         </Button>
-        </div>div>
+        </div>
       {/* <div className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project, index) => (
           <ProjectCard
