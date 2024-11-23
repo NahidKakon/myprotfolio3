ced70bf8a51f76518f64f94e13a5cc49b6539c86
NahidKakon <nahidshakib@outlook.com>
2024-11-23 00:06:38 +0000

    Update AboutSection.jsx
    Signed-off-by: NahidKakon <nahidshakib@outlook.com>

--- app/sections/AboutSection.jsx
+++ app/sections/AboutSection.jsx
@@ -29,7 +29,7 @@ function AboutSection() {
         <h2 className="mb-4 text-3xl font-bold">About Me</h2>
         <p className="mb-4 text-black">
         Recent graduate of an AI-driven Software Engineering bootcamp at Skills City. 
-        <br/>Earlier I did graduation in engineering, during my study I built strong foundation on programming and design through learning GW-Basic, HTML, Delphi, AutoCAD, Gerber, Grafis, Investronica, Photoshop. My passion in IT motivated me doing AISE Bootcamp at Skills City to quickly develop my skills to move into this sector.<br/>As a career changer, transitioning into the IT sector, I am coming with a wealth of transferable skills in decision-making, problem-solving, communication, and teamwork, cultivated through my previous career experiences.
+        <br/>Earlier I did graduation and masters in textile engineering, during my study I built strong foundation on programming and design through learning GW-Basic, HTML, Delphi, AutoCAD, Gerber, Grafis, Investronica, Photoshop. My passion in IT motivated me doing AISE Bootcamp at Skills City to quickly develop my skills to move into this sector.<br/>As a career changer, transitioning into the IT sector, I am coming with a wealth of transferable skills in decision-making, problem-solving, communication, and teamwork, cultivated through my previous career experiences.
 I am bringing a fresh perspective and a strong commitment to success in the tech industry.</p>
         {skills.map((skill, index) => (
           <Progress
